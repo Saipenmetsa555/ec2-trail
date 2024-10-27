e83067c586bd62df64037231e1cd24ba3dea09c7
Saipenmetsa555 <89723740+Saipenmetsa555@users.noreply.github.com>
2024-10-27 09:54:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.get("/", async (req, res) => {
+  const d = "<h1>Hello World!</h1>";
+  res.status(200).send(d);
+});
+
+
 app.listen(port, () => {
   console.log(`SERVER Running at ${port}`);
 });
